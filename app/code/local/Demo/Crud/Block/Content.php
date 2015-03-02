e3e7886266fd78f0a328f531e599d444f8eed121
kirill.bakun <kirill.bakun@hiqo-solutions.com>
2015-03-02 13:41:54 +0300

    module updates

--- app/code/local/Demo/Crud/Block/Content.php
+++ app/code/local/Demo/Crud/Block/Content.php
@@ -1,7 +1,21 @@
 <?php
-class Demo_Crud_Block_Content extends Mage_Core_Block_Template
-{
-    protected function _toHtml() {
-        return 'Demo Crud';
+    class Demo_Crud_Block_Content extends Mage_Core_Block_Template
+    {
+        /*protected function _toHtml() {
+            return 'Demo Crud';
+        }*/
+
+        protected function _construct() {
+            $this->setTemplate('demo/crud/view.phtml');
+        }
+
+        public function getRowUrl(Demo_Crud_Model_Crud $crud) {
+            return $this->getUrl('*/*/view/', array(
+                'id' => $crud->getId()
+            ));
+        }
+
+        public function getCollection() {
+            return Mage::getModel('demo_crud/crud')->getCollection();
+        }
     }
-}
--- app/code/local/Demo/Crud/controllers/IndexController.php
+++ app/code/local/Demo/Crud/controllers/IndexController.php
@@ -4,4 +4,17 @@ class Demo_Crud_IndexController extends Mage_Core_Controller_Front_Action
     public function indexAction() {
         $this->loadLayout()->renderLayout();
     }
+
+    public function viewAction() {
+        $crud_id = (int)$this->getRequest()->getParam('id');
+        if(empty($crud_id)) {
+            $this->_forward('noRoute');
+        }
+
+        $this->loadLayout();
+        $a = $this->getLayout();
+        $b = $a->getBlock('crud.item');
+        $this->getLayout()->getBlock('crud.item')->setCrudId($crud_id);
+        $this->renderLayout();
+    }
 }
